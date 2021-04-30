57b74b2fd9a78fa407a69e3b29aef81856763e14
256217 <80380160+vivek28121997@users.noreply.github.com>
2021-04-30 22:01:48 +0530

    Create Activity_2.h

---
+++ inc/Activity_2.h
@@ -0,0 +1,18 @@
+#ifndef ACTIVITY_2_H_INCLUDED
+#define ACTIVITY_2_H_INCLUDED
+
+/**
+ * @file Activity_2.h
+ * @author (256217)Vivek Kumar Yadav
+ * @brief Initialising pins and channel to ADC conversions for the interfaced peripherals
+ * @version 0.1
+ * @date 2021-04-23
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+uint16_t ReadADC(uint8_t ch);
+void InitADC();
+int Activity_2();
+
+#endif // ACTIVITY_2_H_INCLUDED
