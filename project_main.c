e398bf3513e0b570be024eab739232a9e7aca05d
256217 <80380160+vivek28121997@users.noreply.github.com>
2021-05-01 00:53:46 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,24 +1,21 @@
 /**
- * @file project_main.c
+ * @file main.c
  * @author (256217)Vivek Kumar Yadav
  * @brief Project to define a seat heater systems for passenger occupancy
  * @version 0.1
  * @date 2021-04-29
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
-#include "project_config.h"
 #include "Activity_1.h"
 #include "Activity_2.h"
 #include "Activity_3.h"
 #include "Activity_4.h"
-#include "user_utils.h"
-#include "blinky.h"
 
 /**
  * @brief Initialize all the Peripherals and pin configurations
- * 
+ *
  */
 void peripheral_init(void)
 {
@@ -26,15 +23,9 @@ void peripheral_init(void)
 	//DDRB |= (1 << DDB0);
 }
 
-void change_led_state(uint8_t state)
-{
-	LED_PORT = (state << LED_PIN);
-}
-
-
 /**
  * @brief Main function where the code execution starts
- * 
+ *
  * @return int Return 0 if the program completes successfully
  * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
  * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
@@ -55,16 +46,16 @@ int main(void)
     }
     }
     return 0;
-	/* Initialize Peripherals 
+	/* Initialize Peripherals
 	peripheral_init();
 
 	for(;;)
 	{
         change_led_state(LED_ON);
 		delay_ms(LED_ON_TIME);
-		
+
         change_led_state(LED_OFF);
-		delay_ms(LED_OFF_TIME);	
+		delay_ms(LED_OFF_TIME);
 	}
 	return 0;*/
 }
