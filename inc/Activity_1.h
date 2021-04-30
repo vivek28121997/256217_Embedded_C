7a0d7e572678109f88b9df48d863fd1268f2afb0
256217 <80380160+vivek28121997@users.noreply.github.com>
2021-04-30 23:50:26 +0530

    Update Activity_1.h

--- inc/Activity_1.h
+++ inc/Activity_1.h
@@ -65,4 +65,26 @@ void set_gpio_state(uint8_t port, uint8_t pin, uint8_t state);
 void delay_ms(unsigned int delaytime);
 
 
+#define LED_ON 	(0x01)			/**< LED state HIGH */
+#define LED_OFF	(0x00)			/**< LED state LOW */
+
+#define LED_ON_TIME     (1000)  /**< LED ON time in milli seconds  */
+#define LED_OFF_TIME    (500)   /**< LED OFF time in milli seconds */
+
+/**
+ * Include files
+ */ 
+#include <avr/io.h>
+
+/**
+ * Function Definitions
+ */
+
+/**
+ * @brief Change the state of the LED Pin according to the value of state
+ * 
+ * @param state Pin level to which the LED Pin should be set
+ */
+void change_led_state(uint8_t state);
+
 #endif // ACTIVITY_H_INCLUDED
