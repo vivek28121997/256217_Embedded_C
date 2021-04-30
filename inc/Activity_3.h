391916431c9b8260b5f105bbe0f0871b12f29c1a
256217 <80380160+vivek28121997@users.noreply.github.com>
2021-04-30 22:54:47 +0530

    Create Activity_3.h

---
+++ inc/Activity_3.h
@@ -0,0 +1,18 @@
+#ifndef ACTIVITY_3_H_INCLUDED
+#define ACTIVITY_3_H_INCLUDED
+
+/**
+ * @file Activity_3.h
+ * @author (256217)Vivek Kumar Yadav
+ * @brief Initialising pins and channel to read ADC values for the interfaced peripherals
+ * @version 0.1
+ * @date 2021-04-24
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+uint16_t ReadADC(uint8_t )
+int Activity_3(void);
+
+#endif // ACTIVITY_3_H_INCLUDED
