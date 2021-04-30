4b4b48a1db3f535652068d4b962418d14f77f9d7
256217 <80380160+vivek28121997@users.noreply.github.com>
2021-04-30 23:23:52 +0530

    Create Activity_4.h

---
+++ inc/Activity_4.h
@@ -0,0 +1,20 @@
+#ifndef USART_H_INCLUDED
+#define USART_H_INCLUDED
+
+/**
+ * @file Activity_4.h
+ * @author (256217)Vivek Kumar Yadav
+ * @brief Initialising pins and serial monitor display of temperature as set for the interfaced peripherals
+ * @version 0.1
+ * @date 2021-04-27
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+ 
+void Display(uint16_t )
+void USARTInit(uint16_t );
+char USARTReadChar();
+void USARTWriteChar(char );
+
+#endif // USART_H_INCLUDED
